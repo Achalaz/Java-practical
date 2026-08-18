2dd408dba963b1fccfec0ae24260bd3226274871
Achala Eshan <achalaeshan20@gmail.com>
2026-08-18 23:20:43 +0530

    main method is created

--- testing/class_practice/Car.java
+++ testing/class_practice/Car.java
@@ -3,9 +3,14 @@ public class Car{
     private String model;
     private int year;
 
-    public Car(String barnd,String model,int year){
+    public Car(String brand,String model,int year){
         this.brand=brand;
         this.model=model;
         this.year=year;
     }
+    public void displayInfo(){
+    System.out.println("Brand: "+brand);
+    System.out.println("Model: "+model);
+    System.out.println("Year: "+year);
+}
 }
--- testing/class_practice/Main.java
+++ testing/class_practice/Main.java
@@ -1,1 +1,15 @@
-public class 
+import java.util.Scanner;
+public class Main {
+    public static void main(string[] args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the brand of the car:");
+        String brand=s.nextLine();
+        System.out.println("Enter the model of the car:");
+        String model=s.nextLine();
+        System.out.println("Enter the year of the car:");
+        int year=s.nextInt();
+        Car car=new Car(brand,model,year);
+        car.displayInfo();
+
+    }
+    }
