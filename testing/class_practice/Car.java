677bb7d80eb819c5d52c9f5711ec320b8af34d27
Achala Eshan <achalaeshan20@gmail.com>
2026-08-18 23:16:09 +0530

    car class car constructor is created

---
+++ testing/class_practice/Car.java
@@ -0,0 +1,11 @@
+public class Car{
+    private String brand;
+    private String model;
+    private int year;
+
+    public Car(String barnd,String model,int year){
+        this.brand=brand;
+        this.model=model;
+        this.year=year;
+    }
+}
