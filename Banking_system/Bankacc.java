eb1d9883a88b5b9c85f9ea0c7cac95eadb50bf49
Achala Eshan <achalaeshan20@gmail.com>
2026-08-20 22:16:48 +0530

    Balece class is created😍

--- Banking_system/Bankacc.java
+++ Banking_system/Bankacc.java
@@ -2,4 +2,38 @@ public class Bankacc{
     String accountNum;
     String accountholder;
     double balence;
+//Constuctor 
+    Banckacc(String accontNum,String accountholder){
+        this.accountNum=accontNum;
+        this.accountholder=accountholder;
+        this balence=0;
+    }
+    //deposit
+    public void deposit(double amount){
+        if(amount>0){
+            balence+=amount;
+            System.out.println("Transcation is succsfully completed");
+            System.out.println("________________________________________");
+            System.out.println("Your balence is :: "+balence);
+
+
+        }else{
+            System.out.println("________________________________________");
+            System.out.println("Invalid Amont place enter right amount");
+        }
+    }
+    public void withdraw(double amount){
+        if(balence>=amount){
+            baleance-=amount;
+            System.out.println("Your transaction is succusfully completed");
+            System.out.println("__________________________________________");
+            System.out.println("your current accont ballence is :: "+balence);
+        }else{
+            System.out.println("Your can not get this Money");
+        }
+    }
+    public void display(){
+        System.out.println("____________________________________");
+        System.out.println("Your accont corent balence is :: "+balence);
+    }
 }
